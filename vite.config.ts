5b340b5a8ecdacd5c453821a8580b7bb96b6cd54
Stella Goodrich <goodrist@mail.gvsu.edu>
2025-11-30 18:47:48 -0500

    adding assignment 4 files

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/assignment5-goodrist/",   
   plugins: [vue()],
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler", // <- use modern Dart Sass API
+        api: "modern-compiler", 
       },
     },
   },
